6b642f4f3e21c7aceb1dd01111bf03e00429fa23
vamshins <vamshi.krishna588@gmail.com>
2015-07-31 14:58:27 -0600

    Added custom error messages

--- grails-app/domain/racetrack/Race.groovy
+++ grails-app/domain/racetrack/Race.groovy
@@ -2,6 +2,16 @@ package racetrack
 
 class Race {
 
+    static constraints = {
+        name(blank:false, maxSize:50)
+        startDate(validator: {return (it > new Date())})
+        city()
+        state(inList:["GA", "NC", "SC", "VA"])
+        distance(min:0.0)
+        cost(min:0.0, max:100.0)
+        maxRunners(min:0, max:100000)
+    }
+
     String name
     Date startDate
     String city
@@ -12,8 +22,5 @@ class Race {
 
     static mapping = {
         sort "startDate"
-    }
-
-    static constraints = {
     }
 }
